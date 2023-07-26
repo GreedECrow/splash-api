521c826800b4726f0003309313fe9851fe97db9e
Lee Pierson <awesomehenge@gmail.com>
2023-07-26 11:51:09 +0100

    update

--- server.js
+++ server.js
@@ -10,8 +10,24 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8090;
 
-app.get("/", (request, reponse)=>{
+app.get("/", (request, reponse) => {
     reponse.status(200).json("hello");
 });
 
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
+
+app.get("/photos", async (request, reponse) => {
+    const API = `https:api.unsplash.com/photos/?client_id=${process.env.ACCESS_KEY}&query=goat`
+    const res = await axios.get(API);
+    console.log(res.data);
+    reponse.status(200).json("hello");
+    const photos = res.data.results.map(photo)=>
+            return {
+                id: photo.id,
+                img_url: photo.urls.regular,
+                original_image: photo.links.self,
+                photographer: photo.user.name
+            }
+        })
+        reponse.staus(200).json("hoi hoi");
+});
